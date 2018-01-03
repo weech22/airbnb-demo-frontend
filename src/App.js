8a06160675a637adc0e724b8d88d9e95571b40b1
weech <22.weech@gmail.com>
2018-01-03 22:45:38 +0300

    Styled text elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Header from './styles';
+import React, { Component } from "react";
+import styled from "styled-components";
+import {
+  Title,
+  Price,
+  See,
+  ExploreItemName,
+  ExperienceItemName,
+  Label,
+  HomeItemName,
+  HouseInfo,
+  ResType,
+  ResName,
+  ResPrice,
+  City,
+  Link
+} from "./styles";
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Title>Explore Airbnb</Title>
+        <Price>$29</Price>
+        <See>See all ></See>
+        <ExploreItemName>Homes</ExploreItemName>
+        <ExperienceItemName>Forest Therapy</ExperienceItemName>
+        <Label>44 reviews</Label>
+        <Label>97 · Superhost</Label>
+        <HomeItemName>$82 La Salentina, see, nature & relax</HomeItemName>
+        <HouseInfo>Entire house · 9 beds</HouseInfo>
+        <ResName>Chumley’s</ResName>
+        <ResPrice>About $60 per person</ResPrice>
+        <ResType>Speakeasy</ResType>
+        <City>Paris</City>
+        <Link>Become a host</Link>
+      </div>
+    );
+  }
+}
 
- export default () => {
-  return (
-    <Header>
-      Explore Airbnb
-    </Header>
-  );
-};
+export default App;
--- src/styles.js
+++ src/styles.js
@@ -1,15 +1,127 @@
-import React from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
+import "./font.css";
 
-export const Header = styled.h3`
-  width: 224px;
-  height: 34px;
-
-  font-family: CircularAir;
+export const Title = styled.h3`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
   line-height: 34px;
   font-size: 32px;
 
   color: #383838;
 `;
 
+export const Price = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-weight: Bold;
+  font-size: 15px;
+  line-height: 18px;
+  text-align: Left;
+  color: #383838;
+  padding: 0;
+  margin: 0;
+`;
 
+export const See = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+  line-height: 24px;
+  font-size: 14px;
+  text-align: right;
+
+  color: #383838;
+`;
+
+export const ExploreItemName = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-weight: Bold;
+  line-height: 20px;
+  font-size: 17px;
+  text-align: Left;
+
+  color: #383838;
+`;
+
+export const ExperienceItemName = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+  line-height: 18px;
+  font-size: 15px;
+  text-align: Left;
+
+  color: #383838;
+`;
+
+export const Label = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+
+  line-height: normal;
+  font-size: 12px;
+  text-align: Left;
+
+  color: #383838;
+`;
+
+export const HomeItemName = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+
+  line-height: normal;
+  font-size: 15px;
+  text-align: Left;
+
+  color: #383838;
+`;
+
+export const HouseInfo = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+
+  line-height: normal;
+  font-size: 15px;
+  text-align: Left;
+
+  color: #383838;
+`;
+
+export const ResType = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+
+  line-height: normal;
+  font-size: 10px;
+  text-transform: uppercase;
+
+  color: #383838;
+`;
+
+export const ResName = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+
+  line-height: normal;
+  font-size: 18px;
+
+  color: #383838;
+`;
+
+export const ResPrice = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+  line-height: normal;
+  font-size: 18px;
+
+  color: #383838;
+  mix-blend-mode: normal;
+  opacity: 0.9;
+`;
+
+export const City = styled.p`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+
+  line-height: normal;
+  font-size: 15px;
+
+  color: #383838;
+`;
+
+export const Link = styled.a`
+  font-family: CircularAir, Helvetica Neue, Helvetica, Arial, sans-serif;
+
+  line-height: 24px;
+  font-size: 14px;
+  text-align: center;
+
+  color: #383838;
+`;
