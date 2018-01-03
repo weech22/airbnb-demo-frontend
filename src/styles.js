6cdadad6b9208fe2bed5da7c5cb6e58569a35f4f
weech <22.weech@gmail.com>
2018-01-03 18:49:43 +0300

    Fonts added

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Header = styled.h3`
+  width: 224px;
+  height: 34px;
+
+  font-family: CircularAir;
+  line-height: 34px;
+  font-size: 32px;
+
+  color: #383838;
+`;
+
+
